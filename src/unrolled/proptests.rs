556084a4e58758b51d9cf6af725381c2e93c3bb7
mattwparas <matthewparas2020@u.northwestern.edu>
2021-09-01 21:35:40 -0700

    wip operation matching

--- src/unrolled/proptests.rs
+++ src/unrolled/proptests.rs
@@ -16,18 +16,74 @@ fn list_strategy_from_iterator() -> impl Strategy<Value = List<isize>> {
 // If the state of the resulting is the same AND the invariants of the list hold, we're good
 #[derive(Debug, Clone)]
 enum Action {
-    Cons,
+    Cons(usize),
     Cdr,
     Append(Vec<usize>),
 }
 
+impl Action {
+    fn act_on_vector(self, mut vec: Vec<usize>) -> Vec<usize> {
+        match self {
+            Action::Cons(value) => {
+                vec.insert(0, value);
+                vec
+            }
+            Action::Cdr => {
+                if vec.len() == 0 {
+                    vec
+                } else {
+                    vec.remove(0);
+                    vec
+                }
+            }
+            Action::Append(mut right) => {
+                vec.append(&mut right);
+                vec
+            }
+        }
+    }
+
+    fn act_on_list(self, mut list: List<usize>) -> List<usize> {
+        match self {
+            Action::Cons(value) => {
+                list.cons_mut(value);
+                list
+            }
+            Action::Cdr => list.cdr().unwrap_or(List::new()),
+            Action::Append(right) => list.extend(right),
+        }
+    }
+}
+
+fn crunch_actions_for_vec(mut initial: Vec<usize>, actions: Vec<Action>) -> Vec<usize> {
+    for action in actions {
+        initial = action.act_on_vector(initial);
+    }
+    initial
+}
+
+fn crunch_actions_for_list(mut initial: List<usize>, actions: Vec<Action>) -> List<usize> {
+    for action in actions {
+        initial = action.act_on_list(initial);
+    }
+    initial
+}
+
 fn action_strategy() -> impl Strategy<Value = Action> {
     prop_oneof![
-        Just(Action::Cons),
+        any::<usize>().prop_map(|x| Action::Cons(x)),
         Just(Action::Cdr),
-        prop::collection::vec(0..10000usize, 0..10000)
+        prop::collection::vec(0..100usize, 0..100)
             .prop_map(|x| Action::Append(x.into_iter().collect()))
     ]
+}
+
+fn actions_strategy() -> impl Strategy<Value = Vec<Action>> {
+    prop::collection::vec(action_strategy(), 0..10)
+}
+
+fn vec_strategy() -> impl Strategy<Value = Vec<usize>> {
+    prop::collection::vec(0..10000usize, 0..100)
 }
 
 proptest! {
@@ -71,4 +127,16 @@ proptest! {
             assert_eq!(i, list.get(i).unwrap());
         }
     }
+
+    // #[test]
+    // fn operations_in_order_match(vec in vec_strategy(), actions in actions_strategy()) {
+    //     let initial_list: List<usize> = vec.clone().into_iter().collect();
+
+    //     let resulting_list = crunch_actions_for_list(initial_list, actions.clone());
+    //     let resulting_vector = crunch_actions_for_vec(vec, actions);
+
+    //     for (left, right) in resulting_list.into_iter().zip(resulting_vector.into_iter()) {
+    //         assert_eq!(left, right)
+    //     }
+    // }
 }
