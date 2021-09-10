da7199aa36f23d78c8618b7c147d036fade3d1c7
mattwparas <matthewparas2020@u.northwestern.edu>
2021-09-10 00:04:20 -0500

    refactor benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,43 +5,30 @@ use im_rc::Vector;
 use std::collections::LinkedList;
 
 macro_rules! iteration {
-    (size = $number:expr, $(($func_name:ident, $type:ty)),* $(,)?) => {
+    ($group:expr, size = $number:expr, $(($func_name:ident, $type:ty)),* $(,)?) => {
         $(
-            fn $func_name(c: &mut Criterion) {
-                let list = (0..$number).into_iter().collect::<$type>();
-
-                c.bench_function(stringify!($func_name), |b| {
-                    b.iter(|| black_box((&list).into_iter().sum::<usize>()))
-                });
-            }
+            let list = (0..$number).into_iter().collect::<$type>();
+            $group.bench_function(stringify!($func_name), |b| {
+                b.iter(|| black_box((&list).into_iter().sum::<usize>()))
+            });
         )*
     };
 }
 
-macro_rules! construction {
-    (size = $number:expr, $(($func_name:ident, $type:ty)),* $(,)?) => {
-        $(
-            fn $func_name(c: &mut Criterion) {
-                c.bench_function(stringify!($func_name), |b| {
-                    b.iter(|| black_box((0..$number).into_iter().collect::<$type>()))
-                });
-            }
-        )*
-    }
-}
+pub fn iteration_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iteration");
 
-fn cons_up_list(c: &mut Criterion) {
-    c.bench_function("cons-unrolled-list", |b| {
-        b.iter(|| {
-            let iter = (0..100000usize).into_iter().rev();
-            let last = List::new();
-            black_box(iter.fold(last, |accum, next| List::cons(next, accum)))
-        })
-    });
-}
+    iteration!(
+        group,
+        size = 100000usize,
+        (unrolled_rc_iteration, List<_>),
+        (unrolled_arc_iteration, SharedList<_>),
+        (immutable_vector_iteration, Vector<_>),
+        (vec_iteration, Vec<_>),
+        (linked_list_iteration, LinkedList<_>)
+    );
 
-fn cdr_iteration(c: &mut Criterion) {
-    c.bench_function("unrolled-cdr-iteration", |b| {
+    group.bench_function("unrolled-cdr-iteration", |b| {
         b.iter(|| {
             black_box({
                 let mut list: Option<List<_>> = Some((0..100000usize).into_iter().collect());
@@ -55,36 +42,75 @@ fn cdr_iteration(c: &mut Criterion) {
     });
 }
 
-iteration! {
-    size = 100000usize,
-    (unrolled_rc_iteration, List<_>),
-    (unrolled_arc_iteration, SharedList<_>),
-    (immutable_vector_iteration, Vector<_>),
-    (vec_iteration, Vec<_>),
-    (linked_list_iteration, LinkedList<_>)
+macro_rules! construction {
+    ($group:expr, size = $number:expr, $(($func_name:ident, $type:ty)),* $(,)?) => {
+        $(
+            $group.bench_function(stringify!($func_name), |b| {
+                b.iter(|| black_box((0..$number).into_iter().collect::<$type>()))
+            });
+
+        )*
+    }
 }
 
-construction! {
-    size = 100000usize,
-    (unrolled_rc_construction, List<_>),
-    (immutable_vector_construction, Vector<_>),
-    (vec_construction, Vec<_>)
+pub fn construction_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("construction");
+
+    construction!(
+        group,
+        size = 100000usize,
+        (unrolled_rc_construction, List<_>),
+        (unrolled_arc_construction, SharedList<_>),
+        (immutable_vector_construction, Vector<_>),
+        (vec_construction, Vec<_>),
+        (linked_list_construction, LinkedList<_>)
+    );
+
+    group.bench_function("cons-unrolled-list", |b| {
+        b.iter(|| {
+            let iter = (0..100000usize).into_iter().rev();
+            let last = List::new();
+            black_box(iter.fold(last, |accum, next| List::cons(next, accum)))
+        })
+    });
+}
+
+pub fn push_front_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("push_front");
+
+    group.bench_function("push_front_list", |b| {
+        b.iter(|| {
+            let mut list = List::new();
+            for i in 0..10000 {
+                list.cons_mut(i);
+            }
+        })
+    });
+
+    group.bench_function("push_front_vec", |b| {
+        b.iter(|| {
+            let mut vec = Vec::new();
+            for i in 0..10000 {
+                vec.insert(0, i);
+            }
+        })
+    });
+
+    group.bench_function("push_front_linked_list", |b| {
+        b.iter(|| {
+            let mut vec = LinkedList::new();
+            for i in 0..10000 {
+                vec.push_front(i);
+            }
+        })
+    });
 }
 
 criterion_group!(
     benches,
-    // Iteration
-    unrolled_rc_iteration,
-    unrolled_arc_iteration,
-    cdr_iteration,
-    immutable_vector_iteration,
-    vec_iteration,
-    linked_list_iteration,
-    // Construction
-    unrolled_rc_construction,
-    immutable_vector_construction,
-    vec_construction,
-    cons_up_list,
+    iteration_bench,
+    construction_bench,
+    push_front_bench
 );
 
 criterion_main!(benches);
