1f2cc5f0cbb9f20477c1d87eb8c2ef3728600530
mattwparas <matthewparas2020@u.northwestern.edu>
2023-04-13 08:12:30 -0700

    fix documentation

--- src/list.rs
+++ src/list.rs
@@ -33,7 +33,7 @@ use crate::{
 /// each successive node will grow in size. With `N = 2`, and `G = 2`, the list will look something like this:
 ///
 /// ```text
-/// [0, 1, 2, 3, 4, 5, 6, 7, 8] -> [9, 10, 11, 12] -> [13, 14]
+/// [0, 1, 2, 3, 4, 5, 6, 7] -> [8, 9, 10, 11] -> [12, 13]
 ///
 /// ```
 ///
